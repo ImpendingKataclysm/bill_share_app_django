2d40f9865f68cb3ffe552c4a03bde9ccceb9397e
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-11-15 15:42:10 -0800

    Improved the visual display of form inputs and labels

--- main/forms.py
+++ main/forms.py
@@ -6,22 +6,34 @@ class PartySizeForm(forms.Form):
         label='Party Size:',
         min_value=1,
         max_value=10,
+        widget=forms.NumberInput({
+            'class': 'form-control'
+        })
     )
 
     bill_amount = forms.DecimalField(
         label='Total Amount Due:',
         max_digits=10,
         decimal_places=2,
+        widget=forms.NumberInput({
+            'class': 'form-control'
+        })
     )
 
 
 class PartyMemberForm(forms.Form):
     name = forms.CharField(
         max_length=100,
-        label='Name:'
+        label='Name:',
+        widget=forms.TextInput({
+            'class': 'form-control'
+        })
     )
     days_spent = forms.IntegerField(
         max_value=366,
         min_value=1,
-        label='Days Spent:'
+        label='Days Spent:',
+        widget=forms.NumberInput({
+            'class': 'form-control'
+        })
     )
